1f60d20c88ea35972bacace61fd01ecc021811d5
Pedro Henrique <phsistemasinformacao@gmail.com>
2018-06-01 01:06:56 -0300

    Start Test Extent Reports

---
+++ extentreports/src/main/java/softwareTestingMaterial/ExtentReportsClass.java
@@ -0,0 +1,64 @@
+package softwareTestingMaterial;
+
+import java.io.File;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ExtentReportsClass {
+	ExtentReports extent;
+	ExtentTest logger;
+	
+	@BeforeTest
+	public void startTest() {
+		extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/STMExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Avon.com");
+		extent.addSystemInfo("Environment", "Teste Codigo");
+        extent.addSystemInfo("User Name", "Fabio e Pedro Henrique");
+        extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
+	}
+	 @Test
+	 public void passTest(){
+		 logger = extent.startTest("passTest");
+		 Assert.assertTrue(true);
+		 //To generate the log when the test case is passed
+		 logger.log(LogStatus.PASS, "Test Case Passed is passTest");
+}
+	 @Test
+	 public void failTest(){
+		 logger = extent.startTest("failTest");
+		 Assert.assertTrue(false);
+		 logger.log(LogStatus.PASS, "Test Case (failTest) Status is passed");
+}
+	 @Test
+	 public void skitTest(){
+		 logger = extent.startTest("skipTest");
+		 throw new SkipException("Skipping - This is not ready for testing ");
+}
+	 @AfterMethod
+	 public void getResult(ITestResult result){
+	 if(result.getStatus() == ITestResult.FAILURE){
+	 logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
+	 logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+	 }else if(result.getStatus() == ITestResult.SKIP){
+	 logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+	 }
+	 extent.endTest(logger);
+	 }
+	 @AfterTest
+	 public void endReport(){
+
+	                extent.flush();
+	                extent.close();
+	    }
+	 
+}
